2fc92d9e2b7bebd38ff20376bcffdb6fbb04c037
John Rodolfo <114643834+johnata20@users.noreply.github.com>
2022-10-02 03:43:01 -0600

    Add files via upload

---
+++ Stars.cs
@@ -0,0 +1,16 @@
+﻿int inp;
+
+Console.Write("Enter a number : ");
+inp = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+for (int x = 0; x < inp; x++)
+{
+    for (int y = 0; y < x; y++)
+        Console.Write("*");
+    Console.WriteLine("*");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
